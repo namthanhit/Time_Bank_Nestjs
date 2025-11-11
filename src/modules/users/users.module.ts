ba869c3ad62485094365b26cd074a9f55f70761d
Bùi Ngọc Đức <ducbn@ducbn-2.local>
2025-11-12 00:56:18 +0700

    Fix region

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { RegionService } from '../region/region.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, RegionService],
 })
 export class UsersModule {}
